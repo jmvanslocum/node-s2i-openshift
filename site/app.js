0a7ff5f24d54a7896bda639ea24007c0fa9a7fbe
jmvanslocum <74985513+jmvanslocum@users.noreply.github.com>
2020-11-24 11:54:05 -0700

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,11 +52,11 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
+        "name": "Daniel Bryan",
         "age": 38,
         "gender": "male",
         "street": "34 Main Street",
-        "city": "Toronto",
+        "city": "Seattle",
         "zipcode": "M5H 1T1"
       },
       "medications": ["Metoprolol", "ACE inhibitors", "Vitamin D"],
